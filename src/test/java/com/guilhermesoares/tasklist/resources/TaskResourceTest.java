c71c160be1e924ede2d14bb56a015531beaf5e8f
damnitrumi <guidepaulasoares@hotmail.com>
2024-07-06 17:59:47 -0300

    Tests for taskResource insertTask

--- src/test/java/com/guilhermesoares/tasklist/resources/TaskResourceTest.java
+++ src/test/java/com/guilhermesoares/tasklist/resources/TaskResourceTest.java
@@ -1,9 +1,12 @@
 package com.guilhermesoares.tasklist.resources;
 
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.Matchers.empty;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,8 +26,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.guilhermesoares.tasklist.dto.TaskDTO;
+import com.guilhermesoares.tasklist.dto.TaskRegisterDTO;
 import com.guilhermesoares.tasklist.entities.Task;
 import com.guilhermesoares.tasklist.services.TaskService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 public class TaskResourceTest {
 	
@@ -61,5 +67,28 @@ public class TaskResourceTest {
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$", empty()));
 		
+	}
+	
+	@Test
+	@DisplayName("it should insert a task")
+	void insertTask() throws Exception{
+		TaskRegisterDTO taskRegisterDTO = new TaskRegisterDTO("task", "desc", "HIGH");
+		Task task = new Task(taskRegisterDTO);
+		task.setId(1L);
+		
+		when(taskService.insertTask(eq(taskRegisterDTO), any(HttpServletRequest.class))).thenReturn(task);
+		
+		mockMvc.perform(post("/tasks")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(taskRegisterDTO))
+				.header("Authorization", "Bearer testtoken"))
+		.andExpect(status().isCreated())
+		.andExpect(jsonPath("$.id").value(task.getId()))
+		.andExpect(jsonPath("$.name").value(task.getName()))
+		.andExpect(jsonPath("$.description").value(task.getDescription()))
+		.andExpect(jsonPath("$.createdAt").exists())
+		.andExpect(jsonPath("$.completedAt").value(nullValue()))
+		.andExpect(jsonPath("$.taskStatus").value(task.getTaskStatus().toString()))
+		.andExpect(jsonPath("$.taskPriority").value(task.getTaskPriority().toString()));
 	}
 }
